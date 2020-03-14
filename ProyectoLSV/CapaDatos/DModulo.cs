3b2561e6d1508d09e042f7a9e85bfc838b5ed753
Glamisukii <62161426+Glamisukii@users.noreply.github.com>
2020-03-13 22:17:58 -0300

    Add files via upload

---
+++ ProyectoLSV/CapaDatos/DModulo.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CapaDatos
+{
+   public class DModulo
+    {
+        private int _Idmodulo;
+        private string _Nombre;
+        private string _Descripcion;
+        private string _TextoBuscar;
+
+        public int Idmodulo
+        {
+            get { return _Idmodulo; }
+            set { _Idmodulo = value; }
+        }
+
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set { _Descripcion = value; }
+        }
+
+        public string TextoBuscar
+        {
+            get { return _TextoBuscar; }
+            set { _TextoBuscar = value; }
+        }
+
+        //Constructor Vacio 
+
+        public DModulo()
+        {
+
+        }
+
+        //Constructor con Parametros
+
+        public DModulo(int idmodulo, string nombre, string descripcion, string textobuscar)
+        {
+            this.Idmodulo = idmodulo;
+            this.Nombre = nombre;
+            this.Descripcion = descripcion;
+            this.TextoBuscar = textobuscar;
+        }
+
+        //Metodo Buscar
+
+        public DataTable Buscar(DModulo Modulo)
+        {
+            DataTable DtResultado = new DataTable("Modulo");
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spbuscar_modulo";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 50;
+                ParTextoBuscar.Value = Modulo.TextoBuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            return DtResultado;
+        }
+
+        // Metodo Mostrar
+
+        public DataTable Mostrar()
+        {
+            DataTable DtResultado = new DataTable("Modulo");
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spmostrar_modulo";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            return DtResultado;
+        }
+
+        //Metodo Editar
+
+        public string Editar(DModulo Modulo)
+        {
+
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //codigo
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                //establecer comando
+
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "speditar_modulo";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdmodulo = new SqlParameter();
+                ParIdmodulo.ParameterName = "@IDModulo";
+                ParIdmodulo.SqlDbType = SqlDbType.Int;
+                ParIdmodulo.Value = Modulo.Idmodulo;
+                SqlCmd.Parameters.Add(ParIdmodulo);
+
+                SqlParameter ParNombre = new SqlParameter();
+                ParNombre.ParameterName = "@Nombre";
+                ParNombre.SqlDbType = SqlDbType.VarChar;
+                ParNombre.Size = 30;
+                ParNombre.Value = Modulo.Nombre;
+                SqlCmd.Parameters.Add(ParNombre);
+
+                SqlParameter ParDescripcion = new SqlParameter();
+                ParDescripcion.ParameterName = "@Descripcion";
+                ParDescripcion.SqlDbType = SqlDbType.VarChar;
+                ParDescripcion.Size = 150;
+                ParDescripcion.Value = Modulo.Descripcion;
+                SqlCmd.Parameters.Add(ParDescripcion);
+
+
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "No se Actualizo el Registro";
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+    }
+}
